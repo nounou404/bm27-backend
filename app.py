d7d8ef5825f4b184b6c625bf43b1ba9db197142d
nounou404 <nounoumh176@gmail.com>
2026-05-13 02:27:46 +0100

    update database url

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:210922@localhost/mansati'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:JSpbLoPXKodnxPTBVLiaEgEELcbzsHtn@mysql.railway.internal:3306/railway'
 
 db = SQLAlchemy(app)
 
