90721c7671c470a5ec48c548b2596b0337316f46
nounou404 <nounoumh176@gmail.com>
2026-05-14 10:06:00 +0100

    fix port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -56,4 +57,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+   app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
